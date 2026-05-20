ebaa69d0e9008015126a17ce5164870b5d08b365
camirose-w <camillarose.w@gmail.com>
2026-05-20 21:52:23 +1000

    added flight information feld and button function in flight class updated sort to test new display

--- src/main/searchResults2.js
+++ src/main/searchResults2.js
@@ -1,15 +1,36 @@
 const signInBtn = document.querySelector(".sign-in");
 
+//sign in button selected goes to new page
 if (signInBtn) {
     signInBtn.addEventListener("click", () => {
         window.location.href = "searchBar.html";
     });
 }
 
+//modify search button move back to flight search page
 const modifySearchBtn = document.getElementById("modifySearchBtn");
-
 if (modifySearchBtn) {
     modifySearchBtn.addEventListener("click", () => {
         window.location.href = "searchBar.html";
     });
 }
+
+//filters reset 
+const filters =[
+    document.getElementById("morningCheckbox"),
+    document.getElementById("afternoonCheckbox"),
+    document.getElementById("eveningCheckbox"),
+    document.getElementById("noStopsCheckbox"),
+    document.getElementById("oneStopCheckbox"),
+    document.getElementById("multipleStopsCheckbox")
+];
+const priceRangeSlide = document.getElementById("priceRange");
+
+const resetFilters = document.getElementById("resetFiltersBtn");
+if(resetFilters){
+    resetFilters.addEventListener("click", ()=>{
+        filters.forEach(filter => {
+            filter.checked = true;});
+        priceRangeSlide.value = 600;
+        });
+    }
