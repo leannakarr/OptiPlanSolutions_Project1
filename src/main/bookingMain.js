e100d22a0b68e8b868a166edff1846c18f1b5ff2
camirose-w <camillarose.w@gmail.com>
2026-05-21 12:19:43 +1000

    ABN visability edits

--- src/main/bookingMain.js
+++ src/main/bookingMain.js
@@ -1,4 +1,4 @@
-let loaded = false;
+/*let loaded = false;
 //load flight information
 window.addEventListener("DOMContentLoaded", async () => {
     if (!loaded) {
@@ -8,6 +8,24 @@ window.addEventListener("DOMContentLoaded", async () => {
         const Selectedflight = JSON.parse(sessionStorage.getItem("selectedFlight"));
         if (!SelectedFlight) {
             alert("No flight found.");
-            
+
             return;
-        }}
+        }}})
+*/
+//ABN field is added if select traveling for business 
+const travelForBussines = document.getElementById("travelFoBuss");
+const abnField = document.getElementById("abnField");
+
+travelForBussines.addEventListener("change", () => {
+
+    if (travelForBussines.checked) {
+        abnField.style.display = "flex";
+    } else {
+        abnField.style.display = "none";
+    }
+
+});
+//load with no ABN field
+if (!travelForBussines.checked) {
+    abnField.style.display = "none";
+}
