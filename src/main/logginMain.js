274695c042e949f504f58e441fa67ef5ab2615c9
camirose-w <camillarose.w@gmail.com>
2026-05-22 15:45:29 +1000

    put in login form

--- src/main/logginMain.js
+++ src/main/logginMain.js
@@ -1,3 +1,5 @@
+import User from "../Classes/User.js"
+
 const searchBarBtn = document.getElementById("returnSearchBar");
 if (searchBarBtn) {
     searchBarBtn.addEventListener("click", () => {
@@ -12,3 +14,33 @@ if (signInBtn) {
         window.location.href = "login.html";
     });
 }
+import User from "../Classes/User.js";
+
+document.getElementById("registerBtn").addEventListener("click", () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const user = new User(email, password);
+
+  const message = user.register();
+
+  document.getElementById("result").innerHTML = message;
+});
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const user = User.login(username, password);
+
+  if (user) {
+    document.getElementById("result").innerHTML =
+      `Login Successful<br>Welcome ${user.email}`;
+  } else {
+    document.getElementById("result").innerHTML =
+      `Invalid email or password`;
+  }
+
+});
